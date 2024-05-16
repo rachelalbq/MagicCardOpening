bbb2ba20bae2a42fa4c861b48e160615b91bd76d
rachelalbq <rachel.alb.97@hotmail.com>
2024-05-16 10:58:07 -0300

    deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4VSSEJOW.mjs";import"./chunk-337LVC65.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-35ILOWWW.mjs";import"./chunk-337LVC65.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
